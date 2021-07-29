4baf959002abcef98ed40ed44802ceffc2f82057
nits <nityananda@fullstack.ph>
2021-07-29 17:22:34 +0800

    section reading

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -8,18 +8,45 @@ AppName.Modules.ThemeModule = (function () {
   const _privateMethod = () => {
     // private stuff
 
-    const swiper = new Swiper('.swiper-container', {
+    const swiperBanner = new Swiper('.swiper-container', {
       pagination: {
         el: '.swiper-pagination',
       },
     });
   };
+  const _swiperReading = () => {
+    // private stuff
+
+    const swiperReading = new Swiper('.swiper-reading', {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+    });
+  };
+
+  const _filter = () => {
+    $(document).ready(function(){
+      $('.fa-angle-down').click(function() {
+      $('.filter-dropdown').toggle();
+      })
+      $("filter-dropdown").hover(function() {
+        $('#dropdown-item').css("display", "block");
+        }, function(){
+        $('#dropdown-item').css("display", "none");
+      });
+    })
+  }
 
   /////////////////////
   // Public Methods //
   ///////////////////
   const init = function () {
     _privateMethod();
+    _swiperReading();
+    _filter();
   };
 
   return {
