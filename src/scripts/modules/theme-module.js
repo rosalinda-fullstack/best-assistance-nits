3d54b13a94d78ef28b01f4358ce92143862156bd
nits <nityananda@fullstack.ph>
2021-08-16 16:56:31 +0800

    2000px done, polish all layouts

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -41,34 +41,19 @@ AppName.Modules.ThemeModule = (function () {
     })
   }
  
-   const _headerScroll = () => {
-     $(document).ready(function() {
-  //     $('section-header').on('scroll',function(e){
-  //       $('html,body').animate({
-  //         scrollTop: $($(this).attr('href')).offset().top - 100
-  //       },500);
-  //       e.preventDefault();
-  //     }); $(window).scroll(function() {
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 1){  
-          $('header').addClass("sticky");
-        }
-        else{
-          $('header').removeClass("sticky");
-        }
-      });
+const _headerScroll = () => {
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1){  
+    $('header').addClass("sticky");
+    }
+    else{
+    $('header').removeClass("sticky");
+    }
     });
-   }
+  });
+}
 
-  // window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//     document.getElementById("header").style.fontSize = "30px";
-//   } else {
-//     document.getElementById("header").style.fontSize = "90px";
-//   }
-// }
   /////////////////////
   // Public Methods //
   ///////////////////
